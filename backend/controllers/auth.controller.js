9934961a2d6651dd1d4825b6f5df03eeec9e8f46
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-08-01 14:49:36 +0545

    feat: add email verification functionality during user signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -31,6 +32,8 @@ export const signup = async (req, res) => {
     await user.save();
     //jwt
     generateTokenAndSetCookie(res, user._id);
+
+    await sendVerificationEmail(user.email, user.verificationToken);
 
     return res.status(201).json({
       success: true,
